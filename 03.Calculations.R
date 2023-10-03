f6a2a3a8461ea69de481fbab0b23ebb59fb95be2
nwdecker2 <deckern@myumanitoba.ca>
2023-10-03 11:20:59 -0500

    Made some formatting changes and altered the combining statement at the start. Need Temperature to run still.

--- 03.Calculations.R
+++ 03.Calculations.R
@@ -2,7 +2,7 @@
 ## pH calculation
 
 ## Combines salinity data with pH data
-phconv <- cbind(wk22_pH, wk22_sal)
+phconv <- merge(wk22_pH, wk22_sal, by ="sample_id")
 
 
 ## Creates a new column for and calculates Ionic Strength
@@ -15,18 +15,18 @@ phconv$st <- 0.02824*(phconv$sal/35)
 
 ## Creates a new column for and calculates ln(ks)
 phconv$lnks <- 
-  (-4276.1/phconv$temp) +
+  (-4276.1/(phconv$temp)) +
   141.328 -
-  23.093*ln(phconv$temp) +
-  ((-13856/phconv$temp) +
+  23.093*log(phconv$temp) +
+  ((-13856/(phconv$temp)) +
      324.57 -
-     47.986*ln(phconv$temp))*phconv$I^(1/2) +
-  ((35474/phconv$temp) -
+     47.986*log(phconv$temp))*phconv$I^(1/2) +
+  ((35474/(phconv$temp)) -
      771.54 +
-     114.723*ln(phconv$temp))*phconv$I -
-  (2698/phconv$temp)*phconv$I^(3/2) +
-  (1776/phconv$temp)*phconv$I^2 +
-  ln(1-0.001005*phconv$sal)
+     114.723*log(phconv$temp))*(phconv$I) -
+  (2698/phconv$temp)*(phconv$I)^(3/2) +
+  (1776/phconv$temp)*(phconv$I)^2 +
+  log(1-0.001005*(phconv$sal))
 ## Converts ln(ks) to ks
 phconv$ks <- e^(phconv$lnks)
 
