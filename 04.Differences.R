4fbf57283a08c79f22beb474998055921e44af8f
nwdecker2 <deckern@myumanitoba.ca>
2023-10-11 11:44:45 -0500

    swapped difference direction in 04

--- 04.Differences.R
+++ 04.Differences.R
@@ -5,7 +5,7 @@ ph_comp2 <- ph_comp[complete.cases(ph_comp$pH_T),]
 
 ph_comp2$pH_T <- round(ph_comp2$pH_T, 2)
 
-ph_comp2$difference <-  ph_comp2$pH_T - ph_comp2$pH_f
+ph_comp2$difference <-  ph_comp2$pH_f - ph_comp2$pH_T
 
 summary(ph_comp2$pH_f)
 summary(ph_comp2$pH_T)
